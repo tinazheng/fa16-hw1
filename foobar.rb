83984dd7ccc08c5d826e247515bf6bbbc3b91fe4
Tina Zheng <tinazheng@berkeley.edu>
2016-09-15 19:53:32 -0700

    Finished hw1

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,7 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a.map {|i| i.to_i}.uniq.inject(0) { |sum, i| (i.even? && i < 9) ? sum + i + 2 : sum }
   end
 end
 
